ce9aae73d0181404c81f6afab5110f93d48d10c0
parikhankarim <parikhan.karim@gmail.com>
2023-09-16 20:36:24 +0200

    Add code for printing menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,50 @@
 package org.example;
 
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        String choice;
+
+        do {
+
+            String menu = """
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """;
+
+            System.out.print(menu);
+            choice = Scanner.nextLine();
+
+        } while (!choice.equalsIgnoreCase("e"));
+
+        // Kod som ska köras när användaren väljer att avsluta
     }
 }
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
